420f5415b1518ded9882583c5f7c774f0cca7b37
Dolev Tabibi <dolevtabibi@gmail.com>
2024-03-11 20:31:08 +0200

    FULL FUNCTIONALITY

---
+++ src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let playerNameInput = (
+    <span className="player-initialName">{playerName}</span>
+  );
+  if (isEditing) {
+    playerNameInput = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerNameInput}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
